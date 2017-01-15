c3d89c5b924808410c4619e49c27b8392b8cce5d
GordonQuach <qgordon8@vt.edu>
2017-01-14 23:47:12 -0500

    Add files via upload

---
+++ Problem001A.py
@@ -0,0 +1,35 @@
+import math
+
+m = int(input())
+n = int(input())
+stone_Size = int(input())
+
+# Creating variable for storage.
+loop_Stone_Size = stone_Size
+
+# Amt of stones for n.
+n_Stone = 1
+# Amt of stones for m.
+m_Stone = 1
+# You would start with one stone.
+count_Stone = 1
+
+if (loop_Stone_Size < m):
+	m_Stone += 1
+	count_Stone += 1
+	loop_Stone_Size = loop_Stone_Size*count_Stone
+else:
+	print(str(n_Stone * m_Stone))
+
+# Resets variables for next loop.
+count_Stone = 1
+loop_Stone_Size = stone_Size
+
+if (loop_Stone_Size < n):
+	n_Stone += 1
+	count_Stone += 1
+	loop_Stone_Size = loop_Stone_Size*count_Stone
+else:
+	print(str(n_Stone * m_Stone))
+
+print(str(n_Stone*m_Stone))
